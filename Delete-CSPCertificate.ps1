99c56a334d74d874f9991525ed1ee48ed90c0ba2
Aleksandr Mikoshi <79300692+AleksandrMikoshi@users.noreply.github.com>
2021-02-23 02:16:04 +0300

    Update Delete-CSPCertificate.ps1

--- Delete-CSPCertificate.ps1
+++ Delete-CSPCertificate.ps1
@@ -1,4 +1,6 @@
+#Функция удаления сертификатов с истёкшим сроком действия
+[System.DateTime]$Date = (get-date).AddDays(+1) #Текущая дата +1 день, т.к. файлы сертификатов подменяем заранее
 function Delete-CSPCertificate{
-$Cert = Get-ChildItem Cert:\CurrentUser\My | select -Property * | Where-Object {$_.NotAfter -lt $date} #Выбираем сертификат с истёкшим сроком
+[Selected.System.Security.Cryptography.X509Certificates.X509Certificate2]$Cert = Get-ChildItem Cert:\CurrentUser\My | Select-Object -Property * | Where-Object {$_.NotAfter -lt $Date} #Выбираем сертификат с истёкшим сроком
 Remove-Item -Path $Cert.PSPath -deletekey #Удаляем сертификат и закрытый ключ
 }
