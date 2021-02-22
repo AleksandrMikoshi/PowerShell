d6c31f293096c5ea5f2a055dd4773f93052dc2e9
Aleksandr Mikoshi <79300692+AleksandrMikoshi@users.noreply.github.com>
2021-02-22 04:40:27 +0300

    Create StartDelete.ps1

---
+++ StartDelete.ps1
@@ -0,0 +1,4 @@
+#Запуск функции для удаления сертификатов
+$Function = "Z:\Scripts\Установка сертификатов\Delete-CSPCertificate.ps1" #Путь расположения функции
+. $Function #Добавление функции
+Delete-CSPCertificate #Запуск функции
